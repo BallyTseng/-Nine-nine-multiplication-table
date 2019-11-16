e54cc4b257154e7787b4ddafb9902b330e36b4da
bally <bqddomb@gmail.com>
2019-11-16 22:29:43 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nine_ninemultiplicationtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    var sum = i * j;
+                    Console.Write($"{i}*{j}={sum}");
+                    Console.Write("\t");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
